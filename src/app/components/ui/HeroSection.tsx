9908128f030f179d5f50fba737395bb53cbe4149
AhmedSamy <as8609377@gmail.com>
2025-07-02 00:26:32 +0300

    Add : heroSection

--- src/app/components/ui/HeroSection.tsx
+++ src/app/components/ui/HeroSection.tsx
@@ -19,14 +19,14 @@ export default function HeroSection() {
     { type: "Electric", icon: <FaChargingStation /> },
   ];
   return (
-    <div className="relative w-full h-screen overflow-hidden bottom-17">
-      <Image src="/image/Background.png.png" alt="Car Image" fill className="object-cover" priority />
+    <div className="relative w-full h-screen overflow-hidden bg-white bottom-17">
+      <Image src="/image/Background(66).png" alt="Car Image" fill className="object-cover" priority />
 
       <div className="absolute inset-0 bg-black/50 z-10" />
 
       <div className="absolute inset-0 flex flex-col items-center justify-center text-center text-white px-4 z-20">
         <p className="text-sm md:text-base text-gray-200 mb-2">Find cars for sale and for rent near you</p>
-        <h1 className="text-4xl md:text-6xl font-bold mb-6">Find Your Perfect Car</h1>
+        <h1 className="text-4xl text-white md:text-6xl font-bold mb-6">Find Your Perfect Car</h1>
         <div className="bg-white/90 rounded-xl md:rounded-full py-4 px-4 flex justify-around flex-col md:flex-row gap-3 md:gap-4 items-center max-w-4xl w-full shadow-lg mb-8">
           <select className="text-sm text-black  px-4 py-2 w-full md:w-auto">
             <option>Any Make</option>
--- src/app/home2/page.tsx
+++ src/app/home2/page.tsx
@@ -1,8 +1,8 @@
-// app/home2/page.tsx
+import HeroSection from "../components/ui/HeroSection";
+
+
 export default function Home2() {
   return (
-    <main className="min-h-screen flex justify-center items-center">
-      <h1 className="text-4xl font-bold">Welcome to Home 2</h1>
-    </main>
+    <HeroSection />
   );
 }
