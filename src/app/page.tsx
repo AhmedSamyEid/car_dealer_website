e958aeaa12326646f3da24b4f211c6d470b90933
AhmedSamy <as8609377@gmail.com>
2025-07-02 23:08:56 +0300

    remove ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 
-import { Statistics } from "./components/ui/statistics/Statistics";
-import { WhyChooseUs } from "./components/ui/whychooseus/WhyChooseUs";
-import { PopularMakes } from "./components/ui/popularmakes/PopularMakes";
-import { Testimonials } from "./components/ui/testimonials/Testimonials";
-import { CallToActions } from "./components/ui/callToactions/CallToActions";
-import { BrandsSection } from "./components/ui/brandssection/BrandsSection";
-import { VehiclesSlider } from "./components/ui/vehiclesslider/VehiclesSlider ";
-import { ShopByCategory } from "./components/ui/shopbycategory/ShopByCategory";
-import { LatestBlogPosts } from "./components/ui/latestblogposts/LatestBlogPosts";
+import { Statistics } from "./components/statistics/Statistics";
+import { WhyChooseUs } from "./components/whychooseus/WhyChooseUs";
+import { PopularMakes } from "./components/popularmakes/PopularMakes";
+import { Testimonials } from "./components/testimonials/Testimonials";
+import { CallToActions } from "./components/callToactions/CallToActions";
+import { BrandsSection } from "./components/brandssection/BrandsSection";
+import { VehiclesSlider } from "./components/vehiclesslider/VehiclesSlider ";
+import { ShopByCategory } from "./components/shopbycategory/ShopByCategory";
+import { LatestBlogPosts } from "./components/latestblogposts/LatestBlogPosts";
 import FindYourPerfectCar from "./findYourPerfectCar/HeroSection";
-import { SellYourCarSection } from "./components/ui/sellyourcarsection/SellYourCarSection";
+import { SellYourCarSection } from "./components/sellyourcarsection/SellYourCarSection";
 export default function Home() {
   return (
     <>
