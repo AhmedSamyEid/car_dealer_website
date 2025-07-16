837b789eace6212baed396cce562157a5dc671e3
Your Name <you@example.com>
2025-07-16 23:13:41 +0300

    Add, TheMostSearchedCars in home5

--- src/app/home5/page.tsx
+++ src/app/home5/page.tsx
@@ -1,9 +1,11 @@
 import HeroSection from "@/components/mercedes_benz/HeroSection";
-
+import TheMostSearchedCars from "@/components/themostsearched/TheMostSearched";
 export  default function home5 (){
 return (
   <div>
     <HeroSection />
+    <TheMostSearchedCars />
+  
   </div>
 )
 }
