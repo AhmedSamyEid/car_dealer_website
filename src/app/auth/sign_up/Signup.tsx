76ee6aac79ace46da14c58d114407bc0e7ed3988
Your Name <you@example.com>
2025-07-18 00:38:11 +0300

    Add : Signup

---
+++ src/app/auth/sign_up/Signup.tsx
@@ -0,0 +1,33 @@
+"use client";
+import { useEffect } from "react";
+
+export default function SignUp() {
+  const handleSignUp = () => {
+    const emailInput = document.getElementById("email") as HTMLInputElement;
+    const email = emailInput.value;
+    localStorage.setItem("email", email);
+
+    emailInput.value = "";
+  };
+
+  useEffect(() => {
+    const email = localStorage.getItem("email") || "";
+    const emailInput = document.getElementById("email") as HTMLInputElement;
+    if (emailInput) emailInput.value = email;
+  }, []);
+
+  return (
+    <div className="flex justify-center bg-white py-12">
+      <div className="text-center max-w-md">
+        <h3 className="text-3xl mb-4">Join BoxCar</h3>
+        <p className="text-gray-600 mb-6">Receive pricing updates, shopping tips & more!</p>
+        <div>
+          <input id="email" type="email" placeholder="Your email address" className="border border-gray-300 px-4 py-2 rounded-md w-full" />
+          <button onClick={handleSignUp} className="px-6 py-3 mt-5 bg-blue-600 text-white cursor-pointer rounded-md hover:bg-blue-700 transition">
+            SignUp
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/home5/page.tsx
+++ src/app/home5/page.tsx
@@ -1,11 +1,12 @@
+import SignUp from "../auth/sign_up/Signup";
 import OurTeam from "@/components/Our_team_mercedes/Ourteam";
 import HeroSection from "@/components/mercedes_benz/HeroSection";
 import { WhyChooseUs } from "@/components/why_choose_us/WhyChooseUs";
+import Wemakefinding from "@/components/we_make_finding/Wemakefinding";
 import Onlineinperson from "@/components/online_in_person/Onlineperson";
 import { BrandsSection } from "@/components/brandssection/BrandsSection";
 import { VehiclesSlider } from "@/components/vehiclesslider/VehiclesSlider ";
 import TheMostSearchedCars from "@/components/themostsearched/TheMostSearched";
-import Wemakefinding from "@/components/we_make_finding/Wemakefinding";
 export default function home5() {
   return (
     <div>
@@ -17,6 +18,7 @@ export default function home5() {
       <OurTeam />
       <BrandsSection />
       <Wemakefinding />
+      <SignUp />
     </div>
   );
 }
