da60b86e115b4f301876a7fb9f2d07b18c4b8e8e
Ahmed Samy <as8609377@gmail.com>
2025-07-19 15:25:22 +0300

    Add : VehiclesSlider in home6

--- src/app/home6/page.tsx
+++ src/app/home6/page.tsx
@@ -1,4 +1,6 @@
+import { WhyChooseUs } from "@/components/why_choose_us/WhyChooseUs";
 import { Browsebytype } from "@/components/browseby_type/Browsebytype";
+import { VehiclesSlider } from "@/components/vehiclesslider/VehiclesSlider ";
 import Vehiclesavailable from "@/components/vehicles _available/Vehiclesavailable";
 
 export default function home() {
@@ -6,6 +8,8 @@ export default function home() {
     <div>
       <Vehiclesavailable />
       <Browsebytype />
+      <WhyChooseUs />
+      <VehiclesSlider />
     </div>
   );
 }
