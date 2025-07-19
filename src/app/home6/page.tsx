73b8f77f803deb08dceba9d2f1a6a1eb705e1663
Ahmed Samy <as8609377@gmail.com>
2025-07-19 15:42:11 +0300

    Add :GetFairPrice

--- src/app/home6/page.tsx
+++ src/app/home6/page.tsx
@@ -2,6 +2,7 @@ import { WhyChooseUs } from "@/components/why_choose_us/WhyChooseUs";
 import { Browsebytype } from "@/components/browseby_type/Browsebytype";
 import { VehiclesSlider } from "@/components/vehiclesslider/VehiclesSlider ";
 import Vehiclesavailable from "@/components/vehicles _available/Vehiclesavailable";
+import GetFairPrice from "@/components/get_fair_price/Getfairprice";
 
 export default function home() {
   return (
@@ -10,6 +11,7 @@ export default function home() {
       <Browsebytype />
       <WhyChooseUs />
       <VehiclesSlider />
+      <GetFairPrice />
     </div>
   );
 }
---
+++ src/components/get_fair_price/Getfairprice.tsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+
+export default function GetFairPrice() {
+  return (
+    <div className="flex flex-col md:flex-row justify-around items-center gap-8 p-4">
+      <div className="flex items-center gap-7">
+        <Image src="/image/h711.jpg.png" alt="car" width={150} height={50} />
+        <Image src="/image/h7472.jpg.png" alt="car" width={200} height={100} />
+      </div>
+      <div className="max-w-xl ml-5">
+        <h2 className="text-2xl font-bold mb-2">Get A Fair Price For Your<br /> Car - Sell To Us Today</h2>
+        <p className="mb-4">
+          We are committed to providing our customers with exceptional service, <br />
+          competitive pricing, and a wide range of.
+        </p>
+        <ul className="list-disc list-inside space-y-1">
+          <li>We are the UK’s largest provider, with more patrols in more places</li>
+          <li>You get 24/7 roadside assistance</li>
+          <li>We fix 4 out of 5 cars at the roadside</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
