1703602b8cb3847751b73efd3e16e460e52e0f6e
Ahmed Samy <as8609377@gmail.com>
2025-07-19 16:58:51 +0300

    Add : AutoLoanCalculator && BrandsSection && LatestBlogPosts in home6

--- src/app/home6/page.tsx
+++ src/app/home6/page.tsx
@@ -2,9 +2,12 @@ import { Statistics } from "@/components/statistics/Statistics";
 import GetFairPrice from "@/components/get_fair_price/Getfairprice";
 import { WhyChooseUs } from "@/components/why_choose_us/WhyChooseUs";
 import { Browsebytype } from "@/components/browseby_type/Browsebytype";
+import { BrandsSection } from "@/components/brandssection/BrandsSection";
 import { VehiclesSlider } from "@/components/vehiclesslider/VehiclesSlider ";
+import { LatestBlogPosts } from "@/components/latestblogposts/LatestBlogPosts";
 import Vehiclesavailable from "@/components/vehicles _available/Vehiclesavailable";
 import Whatourcustomers from "@/components/what_our_customers_say/Whatourcustomers";
+import AutoLoanCalculator from "@/components/auto_loan_calculator/Autooanalculator";
 
 export default function home() {
   return (
@@ -16,6 +19,9 @@ export default function home() {
       <GetFairPrice />
       <Statistics />
       <Whatourcustomers />
+      <AutoLoanCalculator />
+      <BrandsSection />
+      <LatestBlogPosts />
     </div>
   );
 }
